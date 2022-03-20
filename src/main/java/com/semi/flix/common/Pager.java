92be6ea62f6a3ad3c2f3a67468e89f3045ed1a02
Khan9389 <idi0tmusic93@gmail.com>
2022-03-20 23:55:59 +0900

    [한우]페이징 기능 수정, EntertDto.board_seq 데이터 타입 int로 수정,

--- src/main/java/com/semi/flix/common/Pager.java
+++ src/main/java/com/semi/flix/common/Pager.java
@@ -22,23 +22,23 @@ public class Pager {
 		
 		// << < 1 2 3 4 5 6 7 8 9 10 > >> 
 		
-		int cpage; //�쁽�옱�럹�씠吏� �젙蹂� - board/list?pg=1  pg 媛믪쓣   cpage濡� 泥섎━�븳�떎
+		int cpage; //현재페이지 정보 - board/list?pg=1  pg 값을   cpage로 처리한다
 		// 12/10 -> ceiling(1.2) -> 2   
-		int pageTotal; //�쟾泥� �럹�씠吏� 媛쒖닔 
-		int pageGroupSize = 5; //洹몃９�쓽 媛쒖닔 - �븳 �솕硫댁뿉 �굹���궪 �럹�씠吏� 理쒕� 媛쒖닔
+		int pageTotal; //전체 페이지 개수 
+		int pageGroupSize = 5; //그룹의 개수 - 한 화면에 나타낼 페이지 최대 개수
 		
 		//1 ~ 5
 		//6 ~ 10
 		//11 ~ 15
-		int pageGroupStart; //1,6,11,16,... 洹몃９�쓽 �떆�옉媛� 
-		int pageGroupEnd;   //5,10,15,....  洹몃９�쓽 醫낅즺媛�
+		int pageGroupStart; //1,6,11,16,... 그룹의 시작값 
+		int pageGroupEnd;   //5,10,15,....  그룹의 종료값
 
 		String path="";
 		//System.out.println(path);
-		String beginLabel 	= "first";//image  �깭洹�
+		String beginLabel 	= "<<";//image  태그
 		String prevLabel 	= "previous"; 
 		String nextLabel 	= "next";
-		String endLabel 	= "last";	
+		String endLabel 	= ">>";	
 
 		
 		try {
@@ -48,15 +48,15 @@ public class Pager {
 			//http://localhost:9000/myhome/freeboard.do?pg=1
 			
 			String page = request.getParameter("pg"); // /board/list?pg=1
-			page = ( page == null ) ? "0" : page;  //null媛� 泥섎━  
+			page = ( page == null ) ? "0" : page;  //null揶쏉옙 筌ｌ꼶�봺  
 			
 
 			cpage = Integer.parseInt(page) ; 
 
 			pageTotal = (int)Math.ceil((total - 1) / pageSize);
-			//�럹吏�吏� 媛쒖닔 : �쟾泥� �뜲�씠�꽣 媛쒖닔 123媛� 
-			//�븳 �럹�씠吏��떦 10媛쒖뵫 : pageSize=10
-			// 123/10 -> 12.3 =>  �삱由� => 13
+			//페지지 개수 : 전체 데이터 개수 123개 
+			//한 페이지당 10개씩 : pageSize=10
+			// 123/10 -> 12.3 =>  올림 => 13
             
 			// 17 /5 - 3 *5  15    20 
 			pageGroupStart = (int) (cpage / pageGroupSize) * pageGroupSize;
@@ -69,23 +69,28 @@ public class Pager {
             //0~4, 5~9, 10~14, 15~19
 				 
 			boolean hasPreviousPage = cpage - pageGroupSize >= 0;
-			//�씠�쟾�럹�씠吏�濡� 媛덇쾶 �엳�뒗媛� �뿬遺�  << < 1 2 3 4 5 > >>
+			//이전페이지로 갈게 있는가 여부  << < 1 2 3 4 5 > >>
 			boolean hasNextPage = pageGroupStart + pageGroupSize < pageTotal;
-			//�떎�쓬�럹�씠吏�濡� 媛� �닔 �엳�뒗媛� �뿬遺� 
+			//다음페이지로 갈 수 있는가 여부 
 			
 			sb.append("<ul class='paginator'>\r\n") ;  
 			
 			//  <<  < 
-			/*첫번째 페이지로
-			 * sb.append((cpage > 0) ? makeLink(0, beginLabel) :
-			 * "<li class='paginator__item paginator__item--prev'><a href='#'><i class='icon ion-ios-arrow-back'></i></a></li>\r\n"
-			 * );
-			 */
-			sb.append(hasPreviousPage ? makeLink(pageGroupStart - 1, prevLabel) : 
-				"<li class='paginator__item paginator__item--prev'><a href='#'><i class='icon ion-ios-arrow-back'></i></a></li>\r\n");
+			//처음으로
+			 sb.append((cpage > 0) ? makeLink(0, beginLabel) :
+			  "<li class='paginator__item paginator__item--prev'><a href=\"javascript:alert('"+"첫번째 페이지 입니다"+"')\">"+beginLabel+"</a></li>\r\n"
+			 );
+			 //뒤로가기 화살표
+			sb.append(hasPreviousPage ? makeLink(pageGroupStart - 1, prevLabel) :
+				(cpage==0 ? 
+				 "<li class='paginator__item paginator__item--prev'><a href=\"javascript:alert('"+"첫번째페이지 입니다"+"')\"><i class='icon ion-ios-arrow-back'></i></a></li>\r\n"
+			
+						:
+				 "<li class='paginator__item paginator__item--prev'><a href=\"javascript:goPage('"+(cpage-1)+"')\"><i class='icon ion-ios-arrow-back'></i></a></li>\r\n"));
+				
 			
 			for (int i = pageGroupStart; i < pageGroupEnd; i++) {
-				if (i == cpage) {//�쁽�옱�럹�씠吏�
+				if (i == cpage) {//현재페이지
 					sb.append(makeActiveLink(i, (i + 1) + "") 
 						
 							);
@@ -94,16 +99,23 @@ public class Pager {
 				}
 			}
 			
-
-			sb.append(hasNextPage ? makeLink(pageGroupEnd, nextLabel) : 
-				"<li class=\"paginator__item paginator__item--next\">\r\n"
-				+ "							<a href=\"#\"><i class=\"icon ion-ios-arrow-forward\"></i></a>\r\n"
-				+ "						</li>\r\n");
-			/*마지막 페이지 넘어가기 버튼
-			 * sb.append((cpage < pageTotal) ? makeLink(pageTotal, endLabel) :
-			 * "<li class=\"page-item\"><a class=\"page-link\"  href='#'>"+endLabel+
-			 * "</a></li>\r\n");
-			 */
+			
+			//앞으로가기 화살표
+			sb.append(hasNextPage ? makeLink(pageGroupEnd, nextLabel) :
+				
+				(cpage==pageTotal ?
+						"<li class=\"paginator__item paginator__item--next\">\r\n"
+						+ "			<a href=\"javascript:alert('"+"마지막페이지입니다"+"')\"><i class=\"icon ion-ios-arrow-forward\"></i></a>\r\n"
+						+ "						</li>\r\n" 
+						:
+						"<li class=\"paginator__item paginator__item--next\">\r\n"
+						+ "							<a href=\"javascript:goPage('"+(cpage+1)+"')\"><i class=\"icon ion-ios-arrow-forward\"></i></a>\r\n"
+						+ "						</li>\r\n"));
+			//마지막으로
+			 sb.append((cpage < pageTotal) ? makeLink(pageTotal, endLabel) :
+				 
+				 "<li class=\"paginator__item\"><a href=\"javascript:alert('"+"마지막 페이지입니다"+"')\">"+endLabel+"</a></li>\r\n");
+			 
 		
 			sb.append("</ul>\r\n") ;  		
 			Tag = sb.toString() ; 	
